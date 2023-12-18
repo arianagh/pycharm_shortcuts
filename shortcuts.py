e43ec141f676411fe83b6be674f60918e6e05ca7
arianagh <arian1999@protonmail.com>
2023-12-18 17:37:23 +0100

    added the shortcuts

---
+++ shortcuts.py
@@ -0,0 +1,72 @@
+"""
+Shortcuts:
+ctrl + left arrow or right -> go on word to word.
+
+ctrl + shift + left arrow or right -> select word to word.
+
+ctrl + shift + up arrow or down -> move the line up or down.
+
+shift + alt + left click of the mouse -> it will select from the cursor to anywhere you want with multiple cursors.
+
+ctrl + ctrl + up arrow or down -> it will add multiple cursors as much as you count the up or down.
+
+alt + j -> it can show and select the occurrences of a selected piece of code as much as you hit it until it finishes.
+
+ctrl + shift + alt + j -> it can show and select all the occurrences of a selected piece of code.
+
+ctrl + tab -> to go to the previous window or select a specific window in pycharm.
+
+shift + f10 -> to run the selected configuration.
+
+ctrl + e -> to see the recently visited files.
+
+ctrl + shift + a -> to search smt in everywhere.
+
+ctrl + space -> to get suggestions about your piece of code.
+
+ctrl + alt + o -> to optimize the imports.
+
+alt + enter -> to get suggestions about import smt or if you have docstring you can inject a selected language in it
+
+ctrl + k -> it will open the version control management for commit and etc.
+
+alt + up arrow or down -> it will move from function to another function.
+
+ctrl + alt + l = to reformat the code in a file or you can select a piece of code and then reformat that.
+
+shift + shift -> it will search for the selected piece of code in everywhere but you can filter where to search too.
+
+ctrl + g -> to go to a specified line of the code.
+
+ctrl + w -> it can extend your selection of a piece of code.
+
+ctrl + shift + j -> if you select for example 3 lines of code it can reformat them all in one line.
+
+ctrl + r -> it will look for the occurrences of a selected piece of code but with better gui than alt + j.
+
+ctrl + y -> it will totally remove a line.
+
+ctrl + backspace -> it will delete word by word.
+
+ctrl + f12 -> it will show the file structure
+
+ctrl + b -> it will show all the usages of piece of code in the project.
+
+alt + shift + left arrow or right to move in between your previous actions.
+
+ctrl + u -> you can see all the super classes of a class.
+
+ctrl + click of the mouse -> it will go the deeper level of your object and implementation.
+
+ctrl + f -> to search in your current file.
+
+ctrl + shift + f -> it will search widely.
+
+ctrl + d -> you can duplicate a piece of code.
+
+Tips:
+Comparing two files:
+1- Select your file
+2- Then press Ctrl + D
+3- Select another file to compare
+"""
